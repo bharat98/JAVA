83d3b7ff13cd28f7d8398b11f6642d2b2c1dbb87
Bharat Gurbaxani <gurbaxanibharat@gmail.com>
2017-04-02 12:11:57 +0530

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,17 @@
+/** Program 3: Apply an Object oriented paradigm using java to develop a stand alone application
+    to   call a method to do the same but from another class.
+    Hint: more than one class needed. 
+    Level: Basic
+*/
+
+/**
+ *
+ * @author Bharat Gurbaxani
+ */
+ 
+public class function {
+    void sum (int a, int b)
+    {int c= a+b;
+    System.out.println(c);
+    }
+}
